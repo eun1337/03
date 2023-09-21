49df0da3e793f8614ff716410546afc19d488b83
eun1337 <dbdmstj0327@naver.com>
2023-09-21 16:31:48 +0900

    04

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-	char c;
+	int a, b;
+	printf("분자를 입력하시오 : ");
+	scanf("%d", &a);
+	printf("분모를 입력하시오 : ");
+	scanf("%d", &b);
 	
-	printf("enter a character : ");
-	scanf("%c", &c);
-	
-	printf("the next charcter of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+	printf("나누기의 결과는 %f 입니다\n", (float)a/b);
 	
 	return 0;
 }
